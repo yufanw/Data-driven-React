2d8245d69d525c895f373096229a76c10cbe7a1a
Yufan Wang <gociousyufan@Yufans-MacBook-Pro.local>
2018-04-17 10:30:03 -0700

    added express server setup

---
+++ server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+let app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+app.listen(3000, () => {
+    console.log("listening on port 3000");
+});
