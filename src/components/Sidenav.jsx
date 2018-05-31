d1cba620e40b7ecd8982afd862f3ca517415aaea
Yufan Wang <yufanwangcn@hotmail.com>
2018-05-31 16:56:22 -0700

    removing feature components

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -4,84 +4,10 @@ import { Button, Collapse, Well } from 'react-bootstrap';
 class Sidenav extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      open: false
-    };
   }
   render() {
-    var sidenav = null;
-    if (window.innerWidth < 480) {
-      sidenav = (
-        <div id="main-nav">
-          <nav id="main">
-            <div className="home-logo">
-              <a href="#" onClick={() => this.setState({ open: !this.state.open })}>
-                <i className="icon-go"></i>
-              </a>
-            </div>
-            <Collapse in={this.state.open}>
-              <ul className="main-bot">
-                <li>
-                  <a href="#" className="nav-icon">
-                    <i className="icon-search"></i>
-                    <p>Search</p>
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="nav-icon">
-                    <i className="icon-feature-family"></i>
-                    <p>Features</p>
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="nav-icon">
-                    <i className="icon-programs"></i>
-                    <p>Programs</p>
-                  </a>
-                </li>
-              </ul>
-            </Collapse>
-          </nav>
-        </div>
-      );
-    } else {
-      sidenav = (
-        <div id="main-nav">
-          <nav id="main">
-            <div className="home-logo">
-              <a href="#" onClick={() => this.setState({ open: !this.state.open })}>
-                <i className="icon-go"></i>
-              </a>
-            </div>
-            <Collapse in={this.state.open}>
-              <ul className="main-bot">
-                <li>
-                  <a href="#" className="nav-icon">
-                    <i className="icon-search"></i>
-                    <p>Search</p>
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="nav-icon">
-                    <i className="icon-feature-family"></i>
-                    <p>Features</p>
-                  </a>
-                </li>
-                <li>
-                  <a href="#" className="nav-icon">
-                    <i className="icon-programs"></i>
-                    <p>Programs</p>
-                  </a>
-                </li>
-              </ul>
-            </Collapse>
-          </nav>
-        </div>
-      );
-    }
     return (
       <div>
-        {sidenav}
       </div> 
     );
   }
--- src/components/Topnav.jsx
+++ src/components/Topnav.jsx
@@ -3,26 +3,11 @@ import React from 'react';
 class Topnav extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      name: "Feature",
-      section: "Create A Feature",
-      icon: "icon-arrow-left"
-    };
   } 
   render() {
     return (
       <header className="primary">
-        <a href="#"><i className={this.state.icon}></i></a>
-        <div className="header-title">
-          <span className="section-name">{this.state.name}</span>
-          <span className="section">{this.state.section}</span>
-        </div>
-        <div className="header-actions">
-          <ul className="actions">
-            <li><a href="#"><i className="icon-clock"></i></a></li>
-            <li><a href="#"><i className="icon-dots"></i></a></li>
-          </ul>
-        </div>
+        <h1>Header</h1>
 
       </header>
     )
